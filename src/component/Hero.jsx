b85377cb1592d646121ff90279d78175a706347e
JOYCE <joychinwenduibeh93@gmail.com>
2024-09-21 02:56:50 +0100

    Add tailwind css

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,40 +1,59 @@
 import {} from "react";
 import MaleDoctor from "../assets/MaleDoctor.png";
-import "../Styles/Header.css";
 
 function Hero() {
   return (
     <>
-      <section className="hero-section">
-        <form className="search-form">
-          <a href="/">
+      <section className="flex justify-between my-0 mx-10 px-20">
+        <form className="max-w-[900px] bg-white p-20 rounded-lg">
+          <a href="/" className="text-[#9fb1e9] text-xl no-underline">
             Please turn on your location to get personalised recommendations.
           </a>
-          <div className="form-grid">
-            <div>
-              <label>State</label>
-              <input type="text" placeholder="Lagos" />
+          <div className="grid grid-cols-2 gap-5 mt-3">
+            <div className="flex flex-col pt-2">
+              <label className="text-[#252525c5] py-1">State</label>
+              <input
+                type="text"
+                placeholder="Lagos"
+                className="p-2 border border-gray-300 rounded-md"
+              />
             </div>
-            <div>
-              <label>City</label>
-              <input type="text" placeholder="Ikeja" />
+            <div className="flex flex-col pt-2">
+              <label className="text-[#252525c5] py-1">City</label>
+              <input
+                type="text"
+                placeholder="Ikeja"
+                className="p-2 border border-gray-300 rounded-md"
+              />
             </div>
-            <div>
-              <label>HMO</label>
-              <input type="text" placeholder="Precious HMO" />
+            <div className="flex flex-col pt-2">
+              <label className="text-[#252525c5] py-1">HMO</label>
+              <input
+                type="text"
+                placeholder="Precious HMO"
+                className="p-2 border border-gray-300 rounded-md"
+              />
             </div>
-            <div>
-              <label>Healthcare Type</label>
-              <input type="text" placeholder="Hospital" />
+            <div className="flex flex-col pt-2">
+              <label className="text-[#252525c5] py-1">Healthcare Type</label>
+              <input
+                type="text"
+                placeholder="Hospital"
+                className="p-2 border border-gray-300 rounded-md"
+              />
             </div>
           </div>
-          <div className="submit-button">
-          <button type="submit" >Search</button>
+          <div className="flex justify-center mt-10">
+            <button
+              type="submit"
+              className="px-10 py-3 bg-[#1E5FC0] text-white rounded-full text-lg hover:bg-[#2448b3] border border-[#1E5FC0]"
+            >
+              Search
+            </button>
           </div>
-
         </form>
         <div className="image1">
-          <img src={MaleDoctor} alt="male doctor" />
+          <img src={MaleDoctor} alt="male doctor" lassName="w-100 h-100 object-cover"/>
         </div>
       </section>
     </>
