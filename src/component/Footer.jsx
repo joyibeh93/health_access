b69ef3711d07555cd18eb4b6ae230ae5deda052c
Keamybams <rulibams16@gmail.com>
2024-09-25 14:39:27 +0100

    footer edit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="mt-16 text-center">
+    <footer className="mt-16 text-center space-y-4">
+      <h2 className="text-black-900 text-lg font-bold">Healthcare Access</h2>
       <p className="text-gray-600">
         Discover personalised healthcare access points nearby.
       </p>
-      <div className="mt-4 flex justify-center space-x-4 text-blue-600 footer-icons">
+      <div className="mt-4 flex justify-center space-x-4 text-black-600 footer-icons">
         <a href="#" className="hover:text-blue-800">
           <i className="fab fa-facebook"></i>
         </a>
@@ -19,6 +20,10 @@ const Footer = () => {
         <a href="#" className="hover:text-blue-800">
           <i className="fab fa-linkedin"></i>
         </a>
+      </div>
+      <div className='mt-4 flex justify-center space-x-2'>
+        <i className="fab fa-solid fa-location-dot"></i>
+        <p className='text-black-600'>Your address</p>
       </div>
     </footer>
   );
