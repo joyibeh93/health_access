264bb2bfc1db0f04c97eb3072df9d92227eb9a90
Keamybams <rulibams16@gmail.com>
2024-09-25 13:21:08 +0100

    added cdn link for icons to index.html

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
